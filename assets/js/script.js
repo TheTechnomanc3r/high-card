13fc9e73277f06876301e434784bdea813a0f3c3
Robin A <robinand@live.com>
2023-02-16 13:30:25 +0000

    style score menu, minor CSS fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,16 +7,47 @@ var cardPath = "assets/images/";
 
 function rndCard() {
 
-        document.getElementById("playercard").innerHTML="";
+    /* Resets the picture */
+    
+    document.getElementById("playercard").innerHTML=""
+
     for (var i = 0; i < 1; i++) {
-        var rnd = cardArr[Math.floor(Math.random() * cardArr.length)];
+        var rndp = cardArr[Math.floor(Math.random() * cardArr.length)];
         var image = new Image();
-        image.src = cardPath+rnd;        
+        image.src = cardPath+rndp;        
         document.getElementById("playercard").appendChild(image);
+        console.log(rndp);
+        
         
    
     }
 
+    document.getElementById("cpucard").innerHTML="";
+
+    for (var i = 0; i < 1; i++) {
+        var rndc = cardArr[Math.floor(Math.random() * cardArr.length)];
+        var image = new Image();
+        image.src = cardPath+rndc;
+        document.getElementById("cpucard").appendChild(image);
+        console.log(rndc, i);
+    }
+
+    if (rndp > rndc) {
+        
+        document.getElementById("playerscore").innerHTML="Player WON!";
+        
+    } else if (rndp < rndc) {
+
+        document.getElementById("cpuscore").innerHTML="CPU WON!";
+    }
+
+}
+
+function playGame() {
+
+
+
+    
 }
 
 
